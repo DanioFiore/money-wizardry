21c39a6126dcca0d34080b9b7f5c7105f3a47cab
Danio <daniofioredev@gmail.com>
2025-08-02 01:01:27 +0200

    refactor: streamline inbound request handling in TelegramController

--- app/Http/Controllers/Api/V1/TelegramController.php
+++ app/Http/Controllers/Api/V1/TelegramController.php
@@ -6,20 +6,38 @@ use App\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Http;
 
 class TelegramController extends Controller
 {
 
     public function inbound(Request $request)
     {
-        $command = CommandsController::extractCommandFromText($request->input('message.text', ''));
+        return ApiResponse::handle(function() use ($request) {
+            // Log the incoming request for debugging
+            Log::info('Telegram Inbound Request', [
+                'request' => $request->all(),
+            ]);
 
-        // handle command
-        if ($command) {
-            return CommandsController::activeCommandActions($command);
-        }
+            if ($request->input('entities.type', '') === 'bot_command') {
+                Log::info('Telegram command detected');
 
+                // Extract the command from the message text
+                $command = CommandsController::extractCommandFromText($request->input('message.text', ''));
+                
+                // Handle the command
+                if (!empty($command)) {
+                    return CommandsController::handleCommands($command);
+                }
+
+            } else {
+                // check the message text to answer
+                Log::info('No command found in the message', [
+                    'message' => $request->input('message.text', ''),
+                ]);
+            }
+
+            return 'success';
+        });
     }
 
 }
