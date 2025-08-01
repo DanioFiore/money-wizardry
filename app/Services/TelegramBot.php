cf290d4616b3816452ce9c5fa6f1da3a1b13fdf9
Danio <daniofioredev@gmail.com>
2025-08-02 01:01:36 +0200

    feat: implement TelegramBot service for sending messages

---
+++ app/Services/TelegramBot.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+/**
+ * TelegramBot
+ */
+class TelegramBot 
+{
+    protected $token;
+    protected $api_endpoint;
+    protected $headers;
+    
+    /**
+     * __construct
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->token = env('TELEGRAM_BOT_TOKEN');
+        $this->api_endpoint = env('TELEGRAM_API_ENDPOINT');
+        $this->setHeaders();
+    }
+    
+    /**
+     * setHeaders
+     *
+     * @return void
+     */
+    protected function setHeaders(){
+        
+        $this->headers = [
+            "Content-Type"  => "application/json",
+            "Accept"        => "application/json",
+        ];
+
+    }
+
+    /**
+     * sendMessage
+     *
+     * @param  string $text
+     * @param  string $chatId
+     * @param  string $replyToMessageId
+     * @return array
+     */
+    public function sendMessage(string $text = '', string $chatId, string $replyToMessageId): array
+    {
+        Log::info('TelegramBot->sendMessage', [
+            'text' => $text,
+            'chatId' => $chatId,
+            'replyToMessageId' => $replyToMessageId,
+        ]);
+        // Default result array
+        $result = ['success' => false, 'body' => []];
+
+        // Create params array
+        $params = [
+            'chat_id' => $chatId,
+            'reply_to_message_id' => $replyToMessageId,
+            'text' => $text,
+        ];
+
+        // Create url -> https://api.telegram.org/bot{token}/sendMessage
+        $url = "{$this->api_endpoint}/bot{$this->token}/sendMessage";
+
+        // Send the request
+        try {
+            
+            $response = Http::withHeaders($this->headers)->post($url,$params);
+            $result = ['success'=>$response->ok(),'body'=>$response->json()];
+
+        } catch (\Throwable $th) {
+
+            $result['error'] = $th->getMessage();
+        }
+
+        Log::info('TelegramBot->sendMessage->result',['result'=>$result]);
+
+        return $result;
+    }
+
+}
