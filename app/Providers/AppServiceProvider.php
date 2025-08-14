a8a60f8e65e3c8476fbd1f5d46b7a33d23b4d169
Danio <daniofioredev@gmail.com>
2025-08-14 20:53:51 +0200

    refactor: change application environment variable from 'production' to 'prod'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,6 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('telegramBot', function ($app) {
             return new \App\Services\TelegramBot();
         });
-
-        if ($this->app->environment(['local', 'testing']) && 
-            class_exists(\Laravel\Pail\PailServiceProvider::class)) {
-            $this->app->register(\Laravel\Pail\PailServiceProvider::class);
-        }
     }
 
     /**
